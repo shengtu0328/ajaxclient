a216adfb5a4b225677f7bbbed36e20d6ec2e0a1c
rqxiao <1007689622@qq.com>
2018-08-24 14:34:34 +0800

    1

---
+++ src/main/java/com/xrq/crossorgin/ajaxclient/ajaxclient/AjaxclientApplication.java
@@ -0,0 +1,12 @@
+package com.xrq.crossorgin.ajaxclient.ajaxclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxclientApplication.class, args);
+    }
+}
